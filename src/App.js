0cf8042af63a6ae3817e99ab45d28d12776a4290
patrickfintan <147137388+patrickfintan@users.noreply.github.com>
2023-11-24 09:18:53 +0000

    Added Comments to App.js
    Added comments explaining the code

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ export default function App() {
   );
   function Co2SavedComponent(props) {
     return (
+      /*
+      - in the below code '1' is being used as a placeholder for the amount of Co2 produced by a person walking
+      - creates a table that displays:
+      - 1. Name of Vehicle: taken directly from the json array
+      - 2. The Co2 it produces: Multiplies distance (taken from props) by the vehicle's multiplier (taken from json array)
+      - 3. The Co2 saved/lost by walking: Multiplies disctance by walking multiplier (which is constant) and then subtracts that result from Co2Produced by Vehicle
+      */
       <>
         <h1>Co2 Saved:</h1>
         <table border="1">
