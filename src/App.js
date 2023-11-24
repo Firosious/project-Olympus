77eb15740210e984f918ccb0f0cf0a7dc248c9e0
patrickfintan <147137388+patrickfintan@users.noreply.github.com>
2023-11-24 09:04:01 +0000

    Update App.js
    Updated code. Now takes in a json array and displays the Vehicle name, Co2 Produced and Co2 saved compared to walking in a table.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./styles.css";
+import { Vehicles } from "./Vehicles";
 
 export default function App() {
-  const [data, setData] = useState([]);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-    <Co2SavedComponent APIData={data} />
+    <>
+      <div className="App">
+        <h1>Sample text 1</h1>
+      </div>
+      <Co2SavedComponent APIData={Vehicles} distance={10} />
+    </>
   );
   function Co2SavedComponent(props) {
     return (
@@ -16,18 +17,22 @@ export default function App() {
         <table border="1">
           <thead>
             <tr>
-              <th>Type</th>
-              <th>Co2Produced</th>
+              <th>Vehicle</th>
+              <th>Co2 Produced</th>
+              <th>Co2 Saved</th>
             </tr>
           </thead>
           <tbody>
             {props.APIData.map((t, index) => (
               <tr key={index}>
                 <td>
-                  <b>{t.type}</b>
+                  <b>{t.name}</b>
                 </td>
                 <td>
-                  <b>{t.co2Produced}</b>
+                  <b>{t.multiplier * props.distance}kg</b>
+                </td>
+                <td>
+                  <b>{t.multiplier * props.distance - 1 * props.distance}kg</b>
                 </td>
               </tr>
             ))}
